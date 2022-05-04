5280922031899a26ed41984e4d50b7ce37097a4e
Kaufmann Dario <dario.kaufmann@students.bfh.ch>
2022-05-04 16:25:16 +0200

    Renamed environment variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ interface ServerConfig {
 }
 
 const server: ServerConfig = {
-    addr:/* process.env.BACKEND_IP ||*/ 'http://192.168.1.130',
+    addr: process.env.REACT_APP_BACKEND_IP || 'localhost',
     port: 5000
 }
 
--- src/services/FileManipulator.tsx
+++ src/services/FileManipulator.tsx
@@ -3,16 +3,13 @@ import axios from "axios";
 import {server} from "../config/config";
 import {DirectoryManipulation} from "../config/types";
 
-const baseURL = `${server.addr}:${server.port}`
+const baseURL = `http://${server.addr}:${server.port}`
 
 function createDirectory(values: DirectoryManipulation){
-    console.log("create a new Folder at", values.basePath, values.name)
     return axios.post(`${baseURL}/api/directory`,values)
-
 }
 
 async  function deleteDirectory(basePath: string){
-    console.log("delete folder at", basePath)
     return axios.delete(`${baseURL}/api/directory`, {
         data: {
             basePath: basePath
