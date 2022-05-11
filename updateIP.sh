9c247f3ce677dd307271350cac8a1d717a0c2ff6
Studer Matthias Lukas <matthiaslukas.studer@students.bfh.ch>
2022-05-11 12:43:01 +0200

    actually fixing the IP issue

--- updateIP.sh
+++ updateIP.sh
@@ -2,13 +2,14 @@
 
 #load the old IP
 cd ~/frontend || exit
-OLDIP=$("cat ip")
+OLDIP=$(cat ~/baseapplication/app/shell/ip)
 
 #load the current IP
-MYIP=$("ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
+##find out the IP of the node
+MYIP=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 if [ -z "$MYIP" ]
 then
-      MYIP=$("ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
+      MYIP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 fi
 
 #compare the two IPs
@@ -16,8 +17,8 @@ if [ "$OLDIP" = "$MYIP" ]; then
     echo "IP is still the same"
 else
     echo "IP has changed"
-    echo "$MYIP" > ip
-    BACKEND_IP=$MYIP pm2 restart backup_front --update-env
+    echo "$MYIP" > ~/baseapplication/app/shell/ip
+    REACT_APP_BACKEND_IP=$MYIP npm run build
 fi
 
 
